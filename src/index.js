87649298f4c7a764bca6b038b790988cc06a8f50
patakib <patakibalintka@gmail.com>
2021-03-13 23:02:31 +0100

    completed project on localhost

--- src/index.js
+++ src/index.js
@@ -9,37 +9,47 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      min: 1,
+      min: 25,
       sec: 0,
       session: 25,
       break: 5,
-      started: 0
+      started: 0,
+      phase: 'session'
     }
     this.breakIncrement = this.breakIncrement.bind(this)
     this.breakDecrement = this.breakDecrement.bind(this)
     this.sessionIncrement = this.sessionIncrement.bind(this)
     this.sessionDecrement = this.sessionDecrement.bind(this)
     this.startSession = this.startSession.bind(this)
+    this.resetSession = this.resetSession.bind(this)
   }
   sessionIncrement = () => {
-    this.setState({
-      session: this.state.session+1
-    })
+    if (this.state.started === 0 & this.state.session < 60) {
+      this.setState({
+        min: this.state.session+1,
+        sec: 0,
+        session: this.state.session+1
+      })
+    }
   }
   sessionDecrement = () => {
-    if (this.state.session > 0) {
+    if (this.state.min > 1 & this.state.started === 0) {
       this.setState({
+        min: this.state.session-1,
+        sec: 0,
         session: this.state.session-1
       })
     }
   }
   breakIncrement = () => {
-    this.setState({
-      break: this.state.break+1
-    })
+    if (this.state.started === 0 & this.state.break < 60) {
+      this.setState({
+        break: this.state.break+1
+      })
+    }
   }
   breakDecrement = () => {
-    if (this.state.break > 0) {
+    if (this.state.break > 1 & this.state.started === 0) {
       this.setState({
         break: this.state.break-1
       })
@@ -57,6 +67,15 @@ class App extends React.Component {
       })
     }
   }
+  resetSession = () => {
+    this.setState({
+      started: 0,
+      session: 25,
+      min: 25,
+      sec: 0,
+      break: 5
+    })
+  }
   myCounter = () => {
     if (this.state.started === 1) {
       if (this.state.sec > 0) {
@@ -70,8 +89,23 @@ class App extends React.Component {
           sec: 59
         })
       }
-      else {
-        // beeper function
+      else if (this.state.min === 0 & this.state.sec === 0 & this.state.phase === 'session') {
+        // beep
+        document.getElementById("beep").play()
+        this.setState({
+          min: this.state.break,
+          sec: 0,
+          phase: 'break'
+        })
+      }
+      else if (this.state.min === 0 & this.state.sec === 0 & this.state.phase === 'break') {
+        // beep
+        document.getElementById("beep").play()
+        this.setState({
+          min: this.state.session,
+          sec: 0,
+          phase: 'session'
+        })
       }
     }
   }
@@ -86,19 +120,25 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h3>Pomodoro Timer</h3>
+        <h1>Pomodoro Timer</h1>
         <div id="timer">
-          <div id="timer-label"></div>
-          <div id="time-left">{this.state.min}:{this.state.sec}</div>
-          <div id="session-length">Session Length: {this.state.session}</div>
-          <div id="session-increment"><button onClick={this.sessionIncrement}>+</button></div>
-          <div id="session-decrement"><button onClick={this.sessionDecrement}>-</button></div>
-          <div id="break-length">Break Length: {this.state.break}</div>
-          <div id="break-increment"><button onClick={this.breakIncrement}>+</button></div>
-          <div id="break-decrement"><button onClick={this.breakDecrement}>-</button></div>
-          <div id="start_stop"><button onClick={this.startSession}>START</button></div>
-          <div id="reset"></div>
-          
+          <div id="timer-label">{this.state.phase}</div>
+          <div id="time-left">{this.state.min < 10 ? "0"+this.state.min : this.state.min}:{this.state.sec < 10 ? "0"+this.state.sec : this.state.sec}</div>
+          <div id="session-label">Session Length: </div>
+          <div id="session-length">{this.state.session}</div>
+          <button id="session-increment" onClick={this.sessionIncrement}>+</button>
+          <button id="session-decrement" onClick={this.sessionDecrement}>-</button>
+          <div id="break-label">Break Length: </div>
+          <div id="break-length">{this.state.break}</div>
+          <button id="break-increment" onClick={this.breakIncrement}>+</button>
+          <button id="break-decrement" onClick={this.breakDecrement}>-</button>
+          <div id="start_stop"><button onClick={this.startSession}>START/PAUSE</button></div>
+          <div id="reset"><button onClick={this.resetSession}>RESET</button></div>
+          <audio
+          id="beep"
+          preload="auto"
+          src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+        />
         </div>
       </div>
     )
